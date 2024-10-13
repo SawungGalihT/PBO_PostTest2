ed61f0283f8b8bd5515225bed218ab60a7ec04d2
Sawung Galih Triatmojo <144757389+SawungGalihT@users.noreply.github.com>
2024-10-13 18:30:15 +0800

    Delete Rumah directory

--- Rumah/src/main/java/Perumahan/interfaces/ManajemenRumah.java
+++
@@ -1,9 +0,0 @@
-
-package Perumahan.interfaces;
-
-public interface ManajemenRumah {
-    void tambahRumah(Perumahan.data.Rumah rumah);
-    void tampilkanRumah();
-    void hapusRumah(int noRumah);
-    void updateRumah(int noRumah);
-}
